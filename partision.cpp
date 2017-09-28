b42f197dd6af6454c76b351c1329bb54332fe4a6
aravind-kumar <karavindkumar1993@gmail.com>
2017-09-28 19:03:20 -0400

    Added the logic for quick_sort

--- partision.cpp
+++ partision.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+
+//Assume that theres a wall on the LHS of the array.
+// And all the elements on the right of the array represent elements greater than pivot
+// And all the elements on the left of the wall represent the elements less than pivot.
+// we start of the wall placed at the left side of the array.
+// we assume that all the elements to the right of the wall are greater than the pivot.
+// and we iterate through the array. at any given time if we find an element less than pivot we 
+/// should be placing it before the wall and incrementing wall.
+/// However this would result in a push of all the elements.
+/// Hence what we do is we swap it with the element next to wall and increment wall.
 void partision(vector<int>& givenInput,int pivotIndex)
 {
    int wall=-1;
