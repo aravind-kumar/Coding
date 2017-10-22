65a5fbd2f87c926bd165566684472c13323660fc
aravind-kumar <karavindkumar1993@gmail.com>
2017-10-21 22:29:06 -0400

    Fixed bug to add remaning elements

--- merge_linked_list.cpp
+++ merge_linked_list.cpp
@@ -19,6 +19,15 @@ void appendNode(Node*& newHead,Node*& tail,Node* addList)
   }
 }
 
+void mergeRemaning(Node* L1,Node*& endList)
+{
+     while(L1!=nullptr)
+     {
+       endList->next = L1;
+       L1 = L1->next; 
+     }
+}
+
 Node* Merge(Node* L1,Node* L2)
 {
    Node* newHead=nullptr;
@@ -38,6 +47,14 @@ Node* Merge(Node* L1,Node* L2)
           L1=L1->next;
           L2=L2->next;
         }
+   if(L1 == nullptr && L2 != nullptr)
+   {
+      MergeRemaning(L2,tail);
+   }
+   else if(L2 == nullptr && L1 != nullptr)
+   {
+      MergeRemaning(L1,tail);
+   }
    return newHead;
 }
 
