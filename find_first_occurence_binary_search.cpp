63c50d9b71fb76a55322cc55023c5e6580df0008
aravind-kumar <karavindkumar1993@gmail.com>
2017-10-27 14:49:40 -0400

    Find first occurence

---
+++ find_first_occurence_binary_search.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+int find_first_occurence(const std::vector<T>& input,const T & k)
+{
+   int low = 0;
+   int high = input.size();
+   while(low< high)
+   {
+      int mid = low + (high-low)/2;
+      if(input[mid] ==k && input[mid-1] != k)
+ 	return mid;
+      else if(input[mid] < k)
+        low = mid+1;
+      else if(input[mid]>=k)
+        high = mid-1;
+   }
+   return -1;    
+}
+
+int main()
+{
+   int num;
+   std::vector<int> inputArray;
+   std::cout<<"\n Enter the number of elements";
+   std::cin>>num;
+   std::cout<<"\n Enter the elements";
+   for(int i=0;i<num;++i)
+   {
+      int givenInput;
+      std::cin>>givenInput;
+      inputArray.emplace_back(givenInput);
+   }
+   std::cout<<"\n Enter the number to be found";
+   std::cin>>num;
+   std::cout<<"\n The first occurence is "<<find_first_occurence(inputArray,num);
+   return 0;
+}
