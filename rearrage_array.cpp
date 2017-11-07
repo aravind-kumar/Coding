f2a1b1d9a21a36efdf64a382f5f40095fbb68fb2
aravind-kumar <karavindkumar1993@gmail.com>
2017-11-06 19:25:59 -0500

    Added descriptions

--- rearrage_array.cpp
+++ rearrage_array.cpp
@@ -9,6 +9,20 @@ using std::cin;
 using std::transform;
 using std::vector;
 
+// Question : Rerrange all the elements in the array such that A[i] =A[A[i]]
+// Every element is would be in the range of 0 to n-1;
+// Catch : We shouldnt replace all the elements with A[i] = A[A[i]]
+//         because it would owerrite the values in the array.
+//         which would mean that we loose the old value and thus wrong output
+//Method1: Would be to create a new array and use the old array to copy all the new values
+//         Without modifying the array.
+//Method2: There is trick we can use to make that the size is O(1)
+//         Note that this method would only work if N*N wouldnt overflow.
+//         We can use a variable to store two value at the same time 
+//         We use the formula A = B + (C % n) *n;
+//			      A % n = C
+// 			      A / n = B
+
 void getInput(vector<int>& input)
 {
    int n;
