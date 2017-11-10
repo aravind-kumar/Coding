f5371735c68f01e485b6e5d25d2173c3a4f353b7
aravind-kumar <karavindkumar1993@gmail.com>
2017-11-10 15:59:57 -0500

    Fixing strongly connected componenets

--- strongly_Connected_Componenets.cpp
+++ strongly_Connected_Componenets.cpp
@@ -16,34 +16,42 @@ using std::function;
 typedef unordered_map<int,vector<int>> graph;
 typedef vector<int> nodes;
 typedef stack<int> finishTimeStack;
-typedef bitset<(1<<32)> visitedNodes(0);
+typedef vector<bool> visitedNodes;
 
 void DFS(const graph& inputGraph,
 	 const int& node,
-         visitedNodes& visited,function<void (int num)> f)
+         visitedNodes& visited,
+         function<void(int)> preHook,
+         function<void(int)> postHook)
 {
+
+  if(preHook)
+    preHook(node);
+
   auto isNotVisited = [visited](int num) { return (visited[num]==false);}; 
   nodes unvisited;
-  nodes::const_iterator it = inputGraph.find(node);
+  auto it = inputGraph.find(node);
 
-  auto end =copy_if((*it).begin(), (*it).end(), unvisited.begin(), isNotVisited);
+  auto end = copy_if(it->second.begin(), it->second.end(), unvisited.begin(), isNotVisited);
           	        
   unvisited.resize(std::distance(unvisited.begin(),end));
   for(auto&& destinationNode : unvisited)
   {
-     visited[destinationNode]=1;
-     DFS(inputGraph,destinationNode,visited);
+     visited[destinationNode]=true;
+     DFS(inputGraph,destinationNode,visited,preHook,postHook);
   }
-  f(node);
+
+  if(postHook)
+      postHook(node);
 }
 
 void getFinishTimes(const graph& g,finishTimeStack inputStack)
 {
-  visitedNodes visted;
+  visitedNodes visited;
   auto addToStack = [&inputStack](int num) { inputStack.push(num);} ;
   for(auto node : g)
   {
-      DFS(g,node.first,visited,addToStack);
+      DFS(g,node.first,visited,nullptr,addToStack);
   }
 }
 
@@ -56,7 +64,7 @@ void runDFSUsing(const graph& g,finishTimeStack inputStack)
    {
      int topOfStack = inputStack.top();
      if(visited[topOfStack] == false)
-     	DFS(g,inputStack.top(),visited,printConnectedComponents);
+     	DFS(g,inputStack.top(),visited,printConnectedComponents,nullptr);
      inputStack.pop();    
    }
 }
@@ -68,10 +76,13 @@ graph getTranspose(graph g)
    {
       for(auto value :key.second) 
       {
-           newGraph[value].push_back(key);  
+        auto it = newGraph.insert
+		   (std::make_pair(value,std::vector<int>(key.first)));
+        if (!it.second)
+        it.first->second.push_back(10); 
       }
    } 
-
+   return newGraph;
 }
 
 void getStronglyConnectedComponents(graph& g,const int& startNode)
