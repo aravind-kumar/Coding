f7a6f4c576841d8f02083d778074662bf401134f
aravind-kumar <karavindkumar1993@gmail.com>
2017-11-18 23:36:37 -0500

    Partial Solution

--- word_break.cpp
+++ word_break.cpp
@@ -3,9 +3,24 @@
 
 bool isWordBreakable(unordered_set<string> map,string input)
 {
-   vector<bool> 
+   vector<bool> result(input.size(),0);
+   result[result.size()-1] = 1;
+   string subString;
+   for(int i=input.size()-1; i>=0;--i)
+   {
+      for(int j=i;j<n;++j)
+      {
+         subString += input[j];
+         result[i] = input.count(subString);
+         if(result[j+1])
+	   break;
+      } 
+
+   }
+   return result[0];
 
 }
+
 int main()
 {
    
