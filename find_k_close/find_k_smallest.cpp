7032cc27bb887de4f487dd897745cd9a2a1a051a
aravind-kumar <karavindkumar1993@gmail.com>
2017-11-22 23:30:46 -0500

    Added explanination

--- find_k_close/find_k_smallest.cpp
+++ find_k_close/find_k_smallest.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+
+// Here we are creating a heap of size n.
+// We are given a list of k sorted elements.
+// where k is the number of elements but each sub-array can inturn have many elements
+// now we can create a max-heap of size sum_of_each subarray.
+// But we only need a heap of size n.
+// We insert the first n elements into the heap and pick the max and insert into the result list.
+// We now would have a problem since each subarray can be of varying sizes how do we know know the 
+// current sub-array size and more importantly how do we know where the max/min element picked belongs to in the input array.
+// 1.Problem : How do we find the subarray and the corresponding position from where the top element came ?
+// 2.Problem : If we found the element how do we know how many more elements are to be iteratreated into ?
+// Solution : We maintain a map with key represeting the subarray index's and its value representing the current elements position which is p// present in the heap.
+// We insert the pair into the heap , wherein the pair's first corresponds to the element and second corresponds to the key of the map.
+//
 template<typname T>
 vector<T> merge_k_sorted_list(const vector<vector<T>>& input)
 {
