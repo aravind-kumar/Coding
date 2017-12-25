2049fecb7710443fcc5249d6b669e18596dda3eb
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-25 17:15:26 -0500

    Check if a tree is balanced

---
+++ check_if_tree_is_balanced/check_if_binary_tree_is_balanced.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+int height(Node* root)
+{
+    if(root)
+    {
+      int lheight = height(root->left);
+      int rheight = height(root->right);
+      return max(lheight,rheight) + 1;
+    }
+    return 0;
+}
+
+bool isBalanced(Node* root)
+{
+   if(root)
+   {
+      int lheight = height(root->left);
+      int rheight = height(root->right);
+      bool result = abs(lheight-rheight)<=1;
+      return result && isBalanced(root->left) && isBalanced(root->right); 
+   }
+   return true;
+}
+
+int main()
+{
+   Node* root = createNode(1);
+   root->left = createNode(2);
+   root->right = createNode(3);
+   root->left->left = createNode(4);
+   root->left->right = createNode(5);
+
+   cout<<isBalanced(root)<<"\n";
+
+   root->right->left = createNode(6);
+   root->right->right = createNode(7);
+
+   cout<<isBalanced(root);
+   
+   return 0;
+}
