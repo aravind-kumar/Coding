c6be84b26e17fc2deddd3347ae27ca3ed898f5ac
aravind-kumar <karavindkumar1993@gmail.com>
2018-03-04 14:02:51 -0500

    Longest substring with k distinct characters

---
+++ longest_substring_with_k_distict_characters/longest_substring_with_k_chars.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        unordered_map<char,int> sMap;
+        int start = 0 ,end = 0,maxLen = INT_MIN,targetCount = 0;
+        
+        while(end<s.length()) {
+            if(sMap[s[end]]==0) {
+                ++targetCount;
+            }
+            sMap[s[end]]++;
+            while(targetCount > k && start<s.length()) {
+                sMap[s[start]]--;
+                if(sMap[s[start]] == 0) {
+                    --targetCount;
+                }
+                
+                ++start;
+            }
+            maxLen = max(maxLen,end-start+1);
+            ++end;
+        }
+
+        return maxLen == INT_MIN ? 0 : maxLen;
+    }
+};
+
+int main() {
+
+
+
+    return 0;
+}
