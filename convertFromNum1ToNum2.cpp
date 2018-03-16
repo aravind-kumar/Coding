95965872448f5d3d5e0e5f680c6b02fad81f32c6
K Aravind Kumar <ezbi.124@gmail.com>
2018-03-15 21:27:52 -0400

    Updated the definition
    Explanined the logic of the code.

--- convertFromNum1ToNum2.cpp
+++ convertFromNum1ToNum2.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 // Find the number of bits needed to convert from num1 to num2
 // We do an xor of num1 and num2 and find the number of bits set.
+// We do an xor of num1 and num2 because we to find the number of bits which are different from num1 and num2
+// We use xor because if the bits are the same the bit is set to 0 and if different then its set to 1 
+// Hence we use xor to find the differnt bits and then we count the bits.
 
 int numOfBitsToConvert(int num1,int num2) {
 
